871366587ccd821d9c831c65b2359d1fbe197a99
TruongVanQuocTrung <1513752@hcmut.edu.vn>
2018-05-14 14:32:19 +0700

    edit to youtuber git server

--- src/server/stt/Status.java
+++
@@ -1,8 +0,0 @@
-package server.stt;
-
-public class Status {
-	public static final String STATUS = "STATUS";
-	public static final String STATUS_SUCCESS = "SUCCESS";
-	public static final String STATUS_FAIL = "FAIL";
-	public static final String STATUS_EXIST = "EXIST";
-}
