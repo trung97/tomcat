c919fb247bef684ffdecde4c7ce79f25b44bf3ee
TruongVanQuocTrung <1513752@hcmut.edu.vn>
2018-05-14 14:55:10 +0700

    copy from git

--- src/lu/lllc/DBInfo.java
+++ src/lu/lllc/DBInfo.java
@@ -3,7 +3,7 @@ package lu.lllc;
 public class DBInfo {
 	static String host = "jws-app-mysql"; // The standard name in Tomcat+ MySQL on openshift
 
-	static String DBname = "dbg24";
+	static String DBname = "lllc";
 	static int port = 3306; // This is for MySQL
 	static String mySQLdbURL = "jdbc:mysql://" + host + ":" + port + "/"
 			+ DBname;
